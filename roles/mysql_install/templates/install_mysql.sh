55c35372e0f62a4837fcfd401a577f9175cf6695
wanghy8166 <wang.hy8166@gmail.com>
2016-08-13 01:02:19 +0800

    init commit

--- roles/mysql_install/templates/install_mysql.sh
+++ roles/mysql_install/templates/install_mysql.sh
@@ -2,12 +2,14 @@
 mv /tmp/my.cnf {{ mysql_datadir }}/my.cnf
 chown -R {{ mysql_user }}:{{ mysql_user }} {{ mysql_datadir }} {{ mysql_basedir }}
 ###init mysql db###"
-{{ mysql_basedir }}/scripts/mysql_install_db --defaults-file={{ mysql_datadir }}/my.cnf --basedir={{ mysql_basedir }} --datadir={{ mysql_datadir }} --user={{ mysql_user }} >> /dev/null 2>&1 &
+{{ mysql_basedir }}/scripts/mysql_install_db --defaults-file={{ mysql_datadir }}/my.cnf --basedir={{ mysql_basedir }} --datadir={{ mysql_datadir }} --user={{ mysql_user }} >> /dev/null 2>&1 
 
-sleep 60 
-/etc/init.d/mysqld start &
+# sleep 30  
+mv {{ mysql_basedir }}/my.cnf {{ mysql_basedir }}/my.cnf.`date +%Y%m%d%H%M%S`
+ 
+/etc/init.d/mysqld start 
 
-sleep 10 
+# sleep 10 
 ln -s -f {{ mysql_basedir }}/bin/mysql /usr/bin/mysql
 ln -s -f {{ mysql_sock }} /tmp/mysql.sock
 chkconfig --add mysqld
