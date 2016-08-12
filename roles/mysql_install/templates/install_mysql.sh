ef86f55bdb4420564b0d642fc5a1ea72b1c3bae2
wanghy8166 <wang.hy8166@gmail.com>
2016-08-12 18:12:59 +0800

    first commit

---
+++ roles/mysql_install/templates/install_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mv /tmp/my.cnf {{ mysql_datadir }}/my.cnf
+chown -R {{ mysql_user }}:{{ mysql_user }} {{ mysql_datadir }} {{ mysql_basedir }}
+###init mysql db###"
+{{ mysql_basedir }}/scripts/mysql_install_db --defaults-file={{ mysql_datadir }}/my.cnf --basedir={{ mysql_basedir }} --datadir={{ mysql_datadir }} --user={{ mysql_user }} >> /dev/null 2>&1 &
+
+sleep 60 
+/etc/init.d/mysqld start &
+
+sleep 10 
+ln -s -f {{ mysql_basedir }}/bin/mysql /usr/bin/mysql
+ln -s -f {{ mysql_sock }} /tmp/mysql.sock
+chkconfig --add mysqld
+check_mysql=$(ps aux|grep mysql|grep -v grep|grep -v ansible|wc -l)
+rm -rf /tmp/$(basename $0)
